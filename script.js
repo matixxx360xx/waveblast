cae0ac4fe72ba9110459613c84731c5c1e4343ee
MSI\mati <matimichalski01@gmail.com>
2025-12-01 23:45:54 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,82 @@
+const player = document.getElementById('Player');
+let playerX = 550;
+let playerY = 330;
+const speed = 5; 
+const playerSize = 90; 
+
+const keys = {
+    w: false,
+    s: false,
+    a: false,
+    d: false
+};
+
+// 1. Nasłuchiwanie na wciśnięcie klawisza (keydown)
+document.addEventListener('keydown', (event) => {
+    const key = event.key.toLowerCase();
+    if (keys.hasOwnProperty(key)) {
+        keys[key] = true;
+        event.preventDefault(); 
+    }
+});
+
+// 2. Nasłuchiwanie na zwolnienie klawisza (keyup)
+document.addEventListener('keyup', (event) => {
+    const key = event.key.toLowerCase();
+    if (keys.hasOwnProperty(key)) {
+        keys[key] = false;
+    }
+});
+
+// 3. Główna pętla
+function gameLoop() {
+
+    let newPlayerX = playerX;
+    let newPlayerY = playerY;
+
+    if(keys.w){
+        newPlayerY -= speed;
+    }
+    if(keys.s){
+        newPlayerY += speed;
+    }
+    if(keys.a){
+        newPlayerX -= speed;
+    }
+    if(keys.d){
+        newPlayerX += speed;
+    }
+
+    const HeightArena = 800;
+    const WidthArena = 1200;
+
+    const minX = 0;
+    const minY = 0;
+    const maxX = WidthArena - playerSize; 
+    const maxY = HeightArena - playerSize; 
+
+    if(newPlayerX < minX){
+        newPlayerX = minX;
+    }
+    if(newPlayerX > maxX){
+        newPlayerX = maxX;
+    }
+    if(newPlayerY < minY){
+        newPlayerY = minY;
+    }
+    if(newPlayerY > maxY){
+        newPlayerY = maxY;
+    }
+
+    playerX = newPlayerX;
+    playerY = newPlayerY;
+
+    player.style.left = playerX + 'px';
+    player.style.top = playerY + 'px';
+
+    //wywołanie tej funkcji ponownie przy następnym renderowaniu
+    requestAnimationFrame(gameLoop);
+}
+
+
+requestAnimationFrame(gameLoop);
